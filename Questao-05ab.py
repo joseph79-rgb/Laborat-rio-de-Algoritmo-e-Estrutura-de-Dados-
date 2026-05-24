0ff59d5131156f34230118c902f93146e3e84549
joseph79-rgb <iarlley.luna@aluno.ufca.edu.br>
2026-05-24 10:54:12 -0300

    Update and rename Questao-05.py to Questao-05ab.py

---
+++ Questao-05ab.py
@@ -0,0 +1,57 @@
+# Alguém e o dobro (Desordenada)
+import random
+
+v1 = []
+t = int(input("Digite o tamanho: "))
+for _ in range(t):
+    v1.append(random.randint(0, 99))
+
+if t <= 0:
+    print("Insira um tamanho valido.")
+else:
+    print(v1)
+    achei = 0
+    for i in range(len(v1)):
+        for j in range(i + 1, len(v1)):
+            if v1[i] == 2 * v1[j] or v1[j] == 2 * v1[i]:
+                print(f"Sim, o par {v1[i]} e {v1[j]}")
+                achei = 1
+                break 
+        
+        if achei == 1:
+            break
+            
+    if achei == 0:
+        print("Não há")
+#Alguém e o dobro (Ordenada)
+import random
+
+v1 = []
+t = int(input("Digite o tamanho: "))
+for _ in range(t):
+    v1.append(random.randint(0, 99))
+
+if t <= 0:
+    print("Insira um tamanho valido.")
+else:
+    v1.sort()
+    print(v1)
+    
+    ja_usados = []
+    paresEncontrados = 0
+    
+    for i in range(len(v1)):
+        if v1[i] in ja_usados:
+            continue
+            
+        for j in range(i + 1, len(v1)):
+            if v1[j] not in ja_usados:
+                if v1[j] == 2 * v1[i]:
+                    print(f"Sim, o par {v1[i]} e {v1[j]}")
+                    ja_usados.append(v1[i])
+                    ja_usados.append(v1[j])
+                    paresEncontrados += 1
+                    break
+    
+    if paresEncontrados == 0:
+        print("Não há")
