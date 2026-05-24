cd91b47e5ed9a5229da7059db4684dcae2accc5f
joseph79-rgb <iarlley.luna@aluno.ufca.edu.br>
2026-05-24 09:45:17 -0300

    Update Questao-01.py

--- Questao-01.py
+++ Questao-01.py
@@ -1,19 +1,19 @@
 #O maior numero impar
-
 import random
 k = int(input("Digite o tamanho do seu vetor:"))
 v = []
 for _ in range(k):
     v.append(random.randint(0,100))
-print(v)
-
-maior = None
-for numero in v:
-    if numero % 2 != 0:
-        maior = numero
-        break
-for i in range(len(v)):
-    if v[i] % 2 != 0 and v[i] > maior:
-        maior = v[i]
-print(f"O maior eh {maior}")
-
+if k <= 0:
+	print("Insira um tamanho valido.")
+else:
+	print(v)
+	maior = None
+	for numero in v:
+	    if numero % 2 != 0:
+	        if maior is None or numero > maior:
+	        	maior = numero
+	if maior is not None:
+		print(f"Maior Impar: {maior}")
+	else: 
+		print("Não há impares")
