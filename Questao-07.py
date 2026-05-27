653f6091a146ac0045dc137d7e466d5f2bd77980
joseph79-rgb <iarlley.luna@aluno.ufca.edu.br>
2026-05-27 08:43:21 -0300

    Create Questao-07.py

---
+++ Questao-07.py
@@ -0,0 +1,18 @@
+import random
+v = []
+t = int(input("Digite o tamanho do seu vetor:"))
+if t <= 0:
+    print("Digite um tamanho valido")
+else:
+    for _ in range(t):
+        v.append(random.randint(0,100))
+    print(v)
+    k = int(input("Digite a distancia:"))
+    achei = 0
+    for i in range(len(v)-k):
+        if v[i] == v[i+k]:
+            print(f"Sim, o {v[i]} nas posições {i} e {i+k}")
+            achei = 1
+            break
+    if achei == 0:
+        print("Não há")
