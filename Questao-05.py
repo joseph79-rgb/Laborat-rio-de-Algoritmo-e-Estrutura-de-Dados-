63b50422f6b9ff8b62a96344dc49c23af7f646ff
joseph79-rgb <iarlley.luna@aluno.ufca.edu.br>
2026-05-27 09:19:20 -0300

    Update Questao-05.py

--- Questao-05.py
+++ Questao-05.py
@@ -7,7 +7,7 @@ if t <= 0:
     print("Digite um tamanho valido")
 else:
     for _ in range(t):
-        v.append(random.randint(0,10))
+        v.append(random.randint(0,100))
     print(v)
     v.sort()
     print(v)
