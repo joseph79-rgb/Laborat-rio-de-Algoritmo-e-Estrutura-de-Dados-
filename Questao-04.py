f769bb423d7b343814ef4feca60703cadb0aa8f2
joseph79-rgb <iarlley.luna@aluno.ufca.edu.br>
2026-05-27 08:58:58 -0300

    Create Questao-04.py

---
+++ Questao-04.py
@@ -0,0 +1,27 @@
+# Elemento Isolado
+
+import random
+v = []
+t = int(input("Digite o tamanho do seu vetor:"))
+if t <= 0:
+    print("Digite um tamanho valido")
+else:
+    for _ in range(t):
+        v.append(random.randint(0,10))
+    print(v)
+    elemento_isolado = None
+    for i in range(len(v)):
+        temVizinho = False
+        for j in range(len(v)):
+            if i!=j:
+                if v[i] == v[j]-1 or v[i] == v[j]+1:
+                    temVizinho = True
+                    break
+        if temVizinho == False:
+            elemento_isolado = v[i]
+            break
+        
+    if elemento_isolado != None:
+        print(f"Sim, há elemento isolado, o {elemento_isolado}")
+    else:
+        print("Não há elemento isolado.")
