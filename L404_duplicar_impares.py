e908e832e9a7b6219c0ca73a11bbdf72212999e2
joseph79-rgb <iarlley.luna@aluno.ufca.edu.br>
2026-08-13 15:16:38 -0300

    Create L404_duplicar_impares.py

---
+++ L404_duplicar_impares.py
@@ -0,0 +1,13 @@
+def duplicar_impares(p):
+    atual = p
+    while atual is not None:
+        if atual.valor % 2 != 0:
+            novo_no = No(atual.valor)
+            novo_no.proximo = atual.proximo
+            atual.proximo = novo_no
+            atual = novo_no.proximo
+        else:
+            atual = atual.proximo
+    return p
+
+# Tempo de execução O(n) devido a percorrer a lista em busca de ímpares.
