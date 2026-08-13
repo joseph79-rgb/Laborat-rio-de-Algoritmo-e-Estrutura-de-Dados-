c3f6663e36cf0c0ef1a5ea2bcec3b9ef83b78d05
joseph79-rgb <iarlley.luna@aluno.ufca.edu.br>
2026-08-13 15:12:46 -0300

    Create L402_separar_pares_impares.py

---
+++ L402_separar_pares_impares.py
@@ -0,0 +1,22 @@
+def separar_pares_impares(inicio):
+    list_impares = No(0)
+    lista_pares = No(0)
+    atual_impar = lista_impares
+    atual_par = lista_pares
+    atual = inicio
+    
+    while atual:
+        if atual.valor % 2 != 0:
+            atual_impar.prox = atual
+            atual_impar = atual_impar.prox
+        else:
+            atual_par.prox = atual
+            atual_par = atual_par.prox
+            atual = atual.prox
+        
+    atual_impar.prox = None
+    atual_par.prox = None
+    
+    return lista_impares.prox, lista_pares.prox
+
+  #Tempo de execução: O(n) porquê foi necessário checar a paridade de cada elemento.
